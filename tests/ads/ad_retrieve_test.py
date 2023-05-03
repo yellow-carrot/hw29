6bbb94a1b63e502b4a3aa7f25543af41512bfbbd
nick <7zx.1zx@gmail.com>
2023-05-03 14:12:16 +0300

    hw31 - ad retrieve test works

--- tests/ads/ad_retrieve_test.py
+++ tests/ads/ad_retrieve_test.py
@@ -8,6 +8,5 @@ from tests.factories import AdFactory
 def test_ad_retrieve(client, access_token):
     ad = AdFactory.create()
     response = client.get(f"/ad/{ad.pk}/", HTTP_AUTHORIZATION=f"Bearer {access_token}")
-    # print(response)
     assert response.status_code == 200
     assert response.data == AdSerializer(ad).data
